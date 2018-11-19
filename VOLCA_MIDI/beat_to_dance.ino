77b0a5d7513e73e76bc2b3d4dc2db3f1991ed501
Ineke Vermeulen <inekevermeulen@gmail.com>
2018-11-19 20:09:56 +0100

    calculate delay

--- VOLCA_MIDI/beat_to_dance.ino
+++ VOLCA_MIDI/beat_to_dance.ino
@@ -2,47 +2,48 @@
 
 #define A_SHAKE A0
 bool inPeak = false;
+unsigned int bpm = 0;
 
 void setup() {
+  inPeak = false;
   // Set MIDI baud rate:
   Serial.begin(31250);
   pinMode(A_SHAKE, INPUT);
+
+  delay(10000);
+  send(NOTE_ON, CLAVES, 127);
+  send(NOTE_OFF, CLAVES, 0);
+  bpm = calculateBpm();
 }
 
 void loop() {
-  int bpm = calculateBpm();
-  inPeak = false;
-  delay(5000);
-
   playRhytm(bpm);
 }
 
 void playRhytm(int bpm) {
+  unsigned int ms_per_minute = (unsigned int)1000*60;
+  unsigned int ms_per_beat = ms_per_minute/bpm;
   for(int i = 0; i < 8; i++) {
       //todo set delay according to bpm
       send(NOTE_ON, KICK, 127);
       send(NOTE_ON, CL_HAT, 127);
-      delay(100);
+      delay(ms_per_beat);
       send(NOTE_OFF, KICK, 0);
       send(NOTE_OFF, CL_HAT, 127);
-      delay(100);
       
       send(NOTE_ON, CL_HAT, 127);
-      delay(100);
+      delay(ms_per_beat);
       send(NOTE_OFF, CL_HAT, 127);
-      delay(100);
       
       send(NOTE_ON, CL_HAT, 127);
       send(NOTE_ON, SNARE, 127);
-      delay(100);
+      delay(ms_per_beat);
       send(NOTE_OFF, CL_HAT, 0);
       send(NOTE_OFF, SNARE, 127);
-      delay(100);
     
       send(NOTE_ON, CL_HAT, 127);
-      delay(100);
+      delay(ms_per_beat);
       send(NOTE_OFF, CL_HAT, 127);
-      delay(100);
     }
 }
 
