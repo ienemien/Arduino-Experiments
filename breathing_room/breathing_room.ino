3d75342e8d3f7e82bed8bdf1510190fdc83e831d
Ineke Vermeulen <inekevermeulen@gmail.com>
2019-08-11 13:51:25 +0200

    control tempo of volca beats through midi clock

--- breathing_room/breathing_room.ino
+++ breathing_room/breathing_room.ino
@@ -6,17 +6,37 @@ MIDI_CREATE_INSTANCE(HardwareSerial, Serial, volcaBeats); // volca beats midi co
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, volcaBass); // volca bass midi connector on TX1
 
 void setup() {
-   volcaBeats.begin(MIDI_CHANNEL_OMNI);
-   volcaBass.begin(MIDI_CHANNEL_OMNI);
+  volcaBeats.begin(MIDI_CHANNEL_OMNI);
+  volcaBass.begin(MIDI_CHANNEL_OMNI);
+  volcaBass.sendRealTime(midi::Start);
 }
 
 void loop() {
-  //test to see if I can send two midi outputs at the same time
-   volcaBeats.sendRealTime(midi::Start);
-   delay(10000);
-   volcaBass.sendRealTime(midi::Start);
-   delay(10000);
-   volcaBeats.sendRealTime(midi::Stop);
-   delay(10000);
-   volcaBass.sendRealTime(midi::Stop);
+  // bass is synced with beats through the sync cable
+  // beats controls the tempo
+  volcaBeats.sendNoteOn(KICK, 100, 1);
+  delay(500);
+  volcaBeats.sendNoteOn(KICK, 100, 1);
+  quarterNotePassed();
+  delay(500);
+  volcaBeats.sendNoteOn(KICK, 100, 1);
+  quarterNotePassed();
+  delay(500);
+  volcaBeats.sendNoteOn(KICK, 100, 1);
+  quarterNotePassed();
+  delay(500);
+  volcaBeats.sendNoteOn(KICK, 100, 1);
+  quarterNotePassed();
+}
+
+void wholeNotePassed() {
+  for (int i = 0; i < 4; i++) {
+    quarterNotePassed();
+  }
+}
+
+void quarterNotePassed() {
+  for (int i = 0; i < 24; i++) {
+    volcaBeats.sendRealTime(midi::Clock);
+  }
 }
--- breathing_room/volca_beats.h
+++ breathing_room/volca_beats.h
@@ -1,26 +1,26 @@
 //note numbers
-#define KICK 36
-#define SNARE 38
-#define LO_TOM 43
-#define HI_TOM 50
-#define CL_HAT 42
-#define OP_HAT 46
-#define CLAP 39
-#define CLAVES 76
-#define AGOGO 67
-#define CRASH 49
+#define  KICK 36
+#define  SNARE 38
+#define  LO_TOM 43
+#define  HI_TOM 50
+#define  CL_HAT 42
+#define  OP_HAT 46
+#define  CLAP 39
+#define  CLAVES 76
+#define  AGOGO 67
+#define  CRASH 49
 
 //part level data
-#define PL_KICK 40
-#define PL_SNARE 41
-#define PL_LO_TOM 42
-#define PL_HI_TOM 43
-#define PL_CL_HAT 44
-#define PL_OP_HAT 45
-#define PL_CLAP 46
-#define PL_CLAVES 47
-#define PL_AGOGO 48
-#define PL_CRASH 49
+#define  PL_KICK 40
+#define  PL_SNARE 41
+#define  PL_LO_TOM 42
+#define  PL_HI_TOM 43
+#define  PL_CL_HAT 44
+#define  PL_OP_HAT 45
+#define  PL_CLAP 46
+#define  PL_CLAVES 47
+#define  PL_AGOGO 48
+#define  PL_CRASH 49
 
 //pcm speed data
 #define PCM_CLAP 50
