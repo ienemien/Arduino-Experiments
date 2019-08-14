1a62a18bd21a07487ccacb846cf2d152ebf2198a
Ineke Vermeulen <inekevermeulen@gmail.com>
2019-08-14 18:45:27 +0200

    get pitch from ultrasonic sensor

--- breathing_room/breathing_room.ino
+++ breathing_room/breathing_room.ino
@@ -1,9 +1,17 @@
 #include <MIDI.h>
+#include <Ultrasonic.h>
 #include "volca_bass.h"
 #include "volca_beats.h"
 
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial, volcaBeats); // volca beats midi connector on TX0
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, volcaBass); // volca bass midi connector on TX1
+
+/*
+   Pass as a parameter the trigger and echo pin, respectively,
+   or only the signal pin (for sensors 3 pins), like:
+   Ultrasonic ultrasonic(13);
+*/
+Ultrasonic ultrasonic(50, 51);
 
 #define VIBRATION_PIN A0
 
@@ -18,16 +26,27 @@ void setup() {
 }
 
 void loop() {
-  int vibration = analogRead(VIBRATION_PIN);
-  int pitch1 = map(vibration, 0, 1023, 0, 255);
-  //volcaBass.sendControlChange(VCO_PITCH_1, pitch1, 1);
-  //volcaBass.sendControlChange(VCO_PITCH_2, pitch1, 1);
-  //volcaBass.sendControlChange(VCO_PITCH_3, pitch1, 1);
-  volcaBass.sendControlChange(LFO_RATE, pitch1, 1);
+
+  volcaBass.sendControlChange(LFO_RATE, getRate(), 1);
+
+  int pitch = getPitch();
+  volcaBass.sendControlChange(VCO_PITCH_1, pitch, 1);
+  volcaBass.sendControlChange(VCO_PITCH_2, pitch, 1);
+  volcaBass.sendControlChange(VCO_PITCH_3, pitch, 1);
 
   // bass is synced with beats through the sync cable
   // beats controls the tempo
   quarterNotePassed();
+}
+
+int getRate() {
+  int vibration = analogRead(VIBRATION_PIN);
+  return map(vibration, 0, 1023, 20, 127);
+}
+
+int getPitch() {
+  int distanceCm = ultrasonic.read();
+  return map(distanceCm, 0, 127, 127, 0);
 }
 
 void quarterNotePassed() {
