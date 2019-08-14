b50603092c113a54966b4fc3819bba83efde15f0
Ineke Vermeulen <inekevermeulen@gmail.com>
2019-08-14 20:14:59 +0200

    set bpm from temperature

--- breathing_room/breathing_room.ino
+++ breathing_room/breathing_room.ino
@@ -1,7 +1,13 @@
 #include <MIDI.h>
 #include <Ultrasonic.h>
+#include <DHT.h>
 #include "volca_bass.h"
 #include "volca_beats.h"
+
+
+#define VIBRATION_PIN A0
+#define DHT11_PIN A1 //temperature + humidity
+#define DHTTYPE DHT11   // DHT 11
 
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial, volcaBeats); // volca beats midi connector on TX0
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, volcaBass); // volca bass midi connector on TX1
@@ -13,12 +19,15 @@ MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, volcaBass); // volca bass midi con
 */
 Ultrasonic ultrasonic(50, 51);
 
-#define VIBRATION_PIN A0
+DHT dht(DHT11_PIN, DHTTYPE);
+
 
 int bpm = 60;
 
 void setup() {
   pinMode(VIBRATION_PIN, INPUT);
+  dht.begin();
+  setBpmFromTemperature();
   volcaBeats.begin(MIDI_CHANNEL_OMNI);
   volcaBass.begin(MIDI_CHANNEL_OMNI);
   volcaBass.sendRealTime(midi::Start);
@@ -26,25 +35,30 @@ void setup() {
 }
 
 void loop() {
+  setBmpFromTemperature();
+  volcaBass.sendControlChange(LFO_RATE, getRateFromVibration(), 1);
 
-  volcaBass.sendControlChange(LFO_RATE, getRate(), 1);
-
-  int pitch = getPitch();
-  volcaBass.sendControlChange(VCO_PITCH_1, pitch, 1);
-  volcaBass.sendControlChange(VCO_PITCH_2, pitch, 1);
-  volcaBass.sendControlChange(VCO_PITCH_3, pitch, 1);
+  volcaBass.sendControlChange(VCO_PITCH_1, getPitchFromDistance(), 1);
+  volcaBass.sendControlChange(VCO_PITCH_2, getPitchFromDistance(), 1);
+  volcaBass.sendControlChange(VCO_PITCH_3, getPitchFromDistance(), 1);
 
   // bass is synced with beats through the sync cable
   // beats controls the tempo
   quarterNotePassed();
 }
 
-int getRate() {
-  int vibration = analogRead(VIBRATION_PIN);
-  return map(vibration, 0, 1023, 20, 127);
+int setBmpFromTemperature() {
+  // Read temperature as Celsius (the default)
+  float temperature = dht.readTemperature();
+  bpm = map(temperature, 15, 30, 50, 160);
 }
 
-int getPitch() {
+int getRateFromVibration() {
+  int vibration = analogRead(VIBRATION_PIN);
+  return map(vibration, 0, 1023, 20, 80);
+}
+
+int getPitchFromDistance() {
   int distanceCm = ultrasonic.read();
   return map(distanceCm, 0, 127, 127, 0);
 }
