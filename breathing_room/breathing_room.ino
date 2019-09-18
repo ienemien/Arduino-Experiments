ce2e81c669527aa65ce730a410d34d96dd11c717
Ineke Vermeulen <inekevermeulen@gmail.com>
2019-09-18 13:48:03 +0200

    play all the beats and melody through the arduino

--- breathing_room/breathing_room.ino
+++ breathing_room/breathing_room.ino
@@ -22,34 +22,103 @@ Ultrasonic ultrasonic(50, 51);
 DHT dht(DHT11_PIN, DHTTYPE);
 
 
-int bpm = 60;
+int bpm = 30;
 
 void setup() {
   pinMode(VIBRATION_PIN, INPUT);
   dht.begin();
-  setBpmFromTemperature();
   volcaBeats.begin(MIDI_CHANNEL_OMNI);
+  volcaBeats.sendRealTime(midi::Stop);
   volcaBass.begin(MIDI_CHANNEL_OMNI);
-  volcaBass.sendRealTime(midi::Start);
-  volcaBeats.sendRealTime(midi::Start);
+  volcaBass.sendRealTime(midi::Stop);
+  //setBpmFromTemperature();
+  //volcaBass.sendRealTime(midi::Start);
+  //volcaBeats.sendRealTime(midi::Start);
 }
 
 void loop() {
-  setBpmFromTemperature();
-  volcaBass.sendControlChange(LFO_RATE, getRateFromVibration(), 1);
+  //setBpmFromTemperature();
+  //volcaBass.sendControlChange(LFO_RATE, getRateFromVibration(), 1);
 
   volcaBass.sendControlChange(VCO_PITCH_1, getPitchFromDistance(), 1);
-  volcaBass.sendControlChange(VCO_PITCH_2, getPitchFromDistance(), 1);
-  volcaBass.sendControlChange(VCO_PITCH_3, getPitchFromDistance(), 1);
+  //volcaBass.sendControlChange(VCO_PITCH_2, getPitchFromDistance(), 1);
+  //volcaBass.sendControlChange(VCO_PITCH_3, getPitchFromDistance(), 1);
 
-  volcaBass.sendControlChange(EG_ATTACK, getAttackFromHumidity(), 1);
-
-  // bass is synced with beats through the sync cable
-  // beats controls the tempo
-  for (int i = 0; i < 4; i++) {
-    quarterNotePassed();
+  //volcaBass.sendControlChange(EG_ATTACK, getAttackFromHumidity(), 1);
+  int business = getBusiness();
+  if (business < 300) {
+    playIdle();
+  } else if (business > 600) {
+    playFast();
+  } else {
+    playMellow();
   }
+}
 
+void playIdle() {
+  bpm = 30;
+  volcaBeats.sendNoteOn(KICK, 80, 1);
+  volcaBass.sendNoteOn(43, 80, 1);
+  eighthNotePassed();
+  volcaBass.sendNoteOff(43, 80, 1);
+  volcaBeats.sendNoteOn(KICK, 80, 1);
+  eighthNotePassed();
+  volcaBeats.sendNoteOn(SNARE, 80, 1);
+  volcaBass.sendNoteOn(43, 80, 1);
+  quarterNotePassed();
+  volcaBass.sendNoteOff(43, 80, 1);
+  volcaBeats.sendNoteOn(KICK, 80, 1);
+  volcaBass.sendNoteOn(38, 80, 1);
+  quarterNotePassed();
+  volcaBass.sendNoteOn(38, 80, 1);
+  volcaBeats.sendNoteOn(SNARE, 80, 1);
+  volcaBass.sendNoteOn(41, 80, 1);
+  quarterNotePassed();
+  volcaBass.sendNoteOff(41, 80, 1);
+}
+
+void playMellow() {
+  bpm = 60;
+  volcaBeats.sendNoteOn(KICK, 80, 1);
+  volcaBass.sendNoteOn(43, 80, 1);
+  eighthNotePassed();
+  volcaBass.sendNoteOff(43, 80, 1);
+  volcaBeats.sendNoteOn(KICK, 80, 1);
+  eighthNotePassed();
+  volcaBeats.sendNoteOn(SNARE, 80, 1);
+  volcaBass.sendNoteOn(43, 80, 1);
+  quarterNotePassed();
+  volcaBass.sendNoteOff(43, 80, 1);
+  volcaBeats.sendNoteOn(KICK, 80, 1);
+  volcaBass.sendNoteOn(38, 80, 1);
+  quarterNotePassed();
+  volcaBass.sendNoteOn(38, 80, 1);
+  volcaBeats.sendNoteOn(SNARE, 80, 1);
+  volcaBass.sendNoteOn(41, 80, 1);
+  quarterNotePassed();
+  volcaBass.sendNoteOff(41, 80, 1);
+}
+
+void playFast() {
+  bpm = 120;
+  volcaBeats.sendNoteOn(KICK, 80, 1);
+  volcaBass.sendNoteOn(43, 80, 1);
+  eighthNotePassed();
+  volcaBass.sendNoteOff(43, 80, 1);
+  volcaBeats.sendNoteOn(KICK, 80, 1);
+  eighthNotePassed();
+  volcaBeats.sendNoteOn(SNARE, 80, 1);
+  volcaBass.sendNoteOn(43, 80, 1);
+  quarterNotePassed();
+  volcaBass.sendNoteOff(43, 80, 1);
+  volcaBeats.sendNoteOn(KICK, 80, 1);
+  volcaBass.sendNoteOn(38, 80, 1);
+  quarterNotePassed();
+  volcaBass.sendNoteOn(38, 80, 1);
+  volcaBeats.sendNoteOn(SNARE, 80, 1);
+  volcaBass.sendNoteOn(41, 80, 1);
+  quarterNotePassed();
+  volcaBass.sendNoteOff(41, 80, 1);
 }
 
 int setBpmFromTemperature() {
@@ -61,17 +130,25 @@ int setBpmFromTemperature() {
 int getAttackFromHumidity() {
   // Read temperature as Celsius (the default)
   float humidity = dht.readHumidity();
-  return map(humidity, 30, 100, 100, 1000);
+  return map(humidity, 30, 100, 0, 127);
 }
 
 int getRateFromVibration() {
   int vibration = analogRead(VIBRATION_PIN);
-  return map(vibration, 0, 1023, 20, 80);
+  int rate = 20;
+  if (vibration > 30) {
+    rate = map(vibration, 0, 1023, 20, 80);
+  }
+  return rate;
+}
+
+int getBusiness() {
+  return analogRead(VIBRATION_PIN);
 }
 
 int getPitchFromDistance() {
   int distanceCm = ultrasonic.read();
-  return map(distanceCm, 0, 127, 127, 0);
+  return map(distanceCm, 0, 127, 21, 80);
 }
 
 void quarterNotePassed() {
@@ -79,6 +156,16 @@ void quarterNotePassed() {
   unsigned int msPerBeat = msPerMinute / bpm;
   int pulseDelay = msPerBeat / 24;
   for (int i = 0; i < 24; i++) {
+    volcaBeats.sendRealTime(midi::Clock);
+    delay(pulseDelay);
+  }
+}
+
+void eighthNotePassed() {
+  unsigned int msPerMinute = (unsigned int)1000 * 60;
+  unsigned int msPerBeat = msPerMinute / bpm;
+  int pulseDelay = msPerBeat / 24;
+  for (int i = 0; i < 12; i++) {
     volcaBeats.sendRealTime(midi::Clock);
     delay(pulseDelay);
   }
